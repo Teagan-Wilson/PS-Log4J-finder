46438f909e57609f7fec164f7800e6263b81f083
Teagan-Wilson <teagan.wilson@shicksolutions.com>
2021-12-16 14:45:08 -0600

    Update PS-Log4J-finder-SharedFolder.ps1
    Fix for env variable in nested string.

--- Network-Folder-Edition/PS-Log4J-finder-SharedFolder.ps1
+++ Network-Folder-Edition/PS-Log4J-finder-SharedFolder.ps1
@@ -11,7 +11,7 @@ del "$Env:TEMP\log4j3.log" -Force;
 del "$Env:TEMP\log4j4.log" -Force;
 
 #Get all LOCAL drives and scan for log4j string in applicable file types. 
-get-wmiobject win32_volume|? {$_.DriveType-eq3}|% {(Get-Psdrive  $_.DriveLetter[0]).Name}|Foreach {$command='findstr /i /s /m "SocketServer.class JndiLookup.class" {0}:\*.jar >> "%TEMP%\log4j1.log" | findstr /i /s /m "SocketServer.class JndiLookup.class" {0}:\*.war >> "%TEMP%\log4j2.log"| findstr /i /s /m "SocketServer.class JndiLookup.class" {0}:\*log4j*.dll  >> "%TEMP%\log4j3.log"| findstr /i /s /m "SocketServer.class JndiLookup.class" {0}:\*.ear >> "%TEMP%\log4j4.log"'-f $_;cmd /c $command};
+get-wmiobject win32_volume|? {$_.DriveType-eq3}|% {(Get-Psdrive  $_.DriveLetter[0]).Name}|Foreach {$command='findstr /i /s /m "SocketServer.class JndiLookup.class" {0}:\*.jar >> "{1}\log4j1.log" | findstr /i /s /m "SocketServer.class JndiLookup.class" {0}:\*.war >> "{1}\log4j2.log" | findstr /i /s /m "SocketServer.class JndiLookup.class" {0}:\*log4j*.dll  >> "{1}\log4j3.log" | findstr /i /s /m "SocketServer.class JndiLookup.class" {0}:\*.ear >> "{1}\log4j4.log"'-f $_, $Env:TEMP;cmd /c $command};
 
 #Get all finds
 $jar=Get-Content -Path ($Env:TEMP + '\log4j1.log');

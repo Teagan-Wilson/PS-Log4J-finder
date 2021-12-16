fc6f2b81d5363d186b2a700b05626a408aa82b39
Teagan-Wilson <teagan.wilson@shicksolutions.com>
2021-12-16 12:30:04 -0600

    Added Scaning for .EAR files with log4j in them.
    Added Scan for .EAR files with log4j in them.

--- Network-Folder-Edition/PS-Log4J-finder-SharedFolder.ps1
+++ Network-Folder-Edition/PS-Log4J-finder-SharedFolder.ps1
@@ -9,13 +9,14 @@ del "%TEMP%\log4j1.log";
 del "%TEMP%\log4j2.log";
 del "%TEMP%\log4j3.log";
 
-#Get all LOCAL drives and scan for log4j string in applicatble file types. 
-get-wmiobject win32_volume|? {$_.DriveType-eq3}|% {(Get-Psdrive  $_.DriveLetter[0]).Name}|Foreach {$command='findstr /i /s /m "SocketServer.class JndiLookup.class" {0}:\*.jar >> "%TEMP%\log4j1.log" | findstr /i /s /m "SocketServer.class JndiLookup.class" {0}:\*.war >> "%TEMP%\log4j2.log"| findstr /i /s /m "SocketServer.class JndiLookup.class" {0}:\*log4j*.dll  >> "%TEMP%\log4j3.log"'-f $_;cmd /c $command};
+#Get all LOCAL drives and scan for log4j string in applicable file types. 
+get-wmiobject win32_volume|? {$_.DriveType-eq3}|% {(Get-Psdrive  $_.DriveLetter[0]).Name}|Foreach {$command='findstr /i /s /m "SocketServer.class JndiLookup.class" {0}:\*.jar >> "%TEMP%\log4j1.log" | findstr /i /s /m "SocketServer.class JndiLookup.class" {0}:\*.war >> "%TEMP%\log4j2.log"| findstr /i /s /m "SocketServer.class JndiLookup.class" {0}:\*log4j*.dll  >> "%TEMP%\log4j3.log"| findstr /i /s /m "SocketServer.class JndiLookup.class" {0}:\*.ear >> "%TEMP%\log4j4.log"'-f $_;cmd /c $command};
 
 #Get all finds
 $jar=Get-Content -Path ($Env:TEMP + '\log4j1.log');
 $war=Get-Content -Path ($Env:TEMP + '\log4j2.log');
 $dll=Get-Content -Path ($Env:TEMP + '\log4j3.log');
+$ear=Get-Content -Path ($Env:TEMP + '\log4j4.log');
 
 #Check for finds and copy files to network location
 if($jar){
@@ -32,6 +33,11 @@ if($dll){
 Get-Content -Path ($Env:TEMP + '\log4j3.log') | Out-file ($networklocation + '\' + $Env:COMPUTERNAME + '-log4j-dll-FOUND.log')
 } else {
 Get-Content -Path ($Env:TEMP + '\log4j3.log') | Out-file  ($networklocation + '\' + $Env:COMPUTERNAME + '-log4j-dll-NOTF.log')
+}
+if($ear){
+Get-Content -Path ($Env:TEMP + '\log4j4.log') | Out-file ($networklocation + '\' + $Env:COMPUTERNAME + '-log4j-ear-FOUND.log')
+} else {
+Get-Content -Path ($Env:TEMP + '\log4j4.log') | Out-file  ($networklocation + '\' + $Env:COMPUTERNAME + '-log4j-ear-NOTF.log')
 }
 
 
